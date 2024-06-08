ca5ec0c10fabe0e05579859671bab53d75f79e84
josephl17 <joseph.lauria17@gmail.com>
2024-06-08 02:01:49 -0500

    solution and test file updates

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,9 +1,27 @@
-function toRomanLazy(num) {
-  return "";
-}
+const toRomanLazy = (num) => {
+  let output = ""
+  let romanNumeralToArabic = {
+  'I': 1,
+  'V': 5,
+  'X': 10,
+  'L': 50,
+  'C': 100,
+  'D': 500,
+  'M': 1000
+  }
 
-function toRoman(num) {
-  return "";
-}
+  const romanNumeralOrder = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
 
-module.exports = { toRoman, toRomanLazy };
+  for (let romanNumeral of romanNumeralOrder) {
+      const arabicEquivalent = romanNumeralToArabic[romanNumeral]
+      const count = Math.floor(num / arabicEquivalent)
+      output += romanNumeral.repeat(count)
+      num -= count * arabicEquivalent
+  }
+
+  return output
+
+};
+
+
+module.exports = { toRomanLazy }
